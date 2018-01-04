5f11b8795dfb782aa7583c9c2774558453ee0294
ask2tam <34500750+ask2tam@users.noreply.github.com>
2018-01-04 12:55:45 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-#./start.sh gcloud-cryptomine/pools/itns_1cpu_b 14 16
-#./start.sh gcloud-cryptomine/pools/itns_8cpu_b 560 640
+#gcloud-cryptomine/start.sh gcloud-cryptomine/pools/itns_1cpu_b 14 16
+#gcloud-cryptomine/start.sh gcloud-cryptomine/pools/itns_8cpu_b 560 640
 
 CONFIG_FILE=$1
 min=$2
 max=$3
 
+sudo sysctl -w vm.nr_hugepages=128
+
 while true
 do
 for i in `seq 1 120`
 do
-	if [ "`top -n1 | grep xmr-stak-cpu`" = "" ]
+	if [ "` top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' `" = "" ]
 	then 
 	nohup cpulimit -l $max gcloud-cryptomine/bin/xmr-stak-cpu $CONFIG_FILE &
 	fi
 	sleep 10
-	top -b -n 1 | head | grep xmr-stak-cpu | cut -f1 -d" " | xargs cpulimit -l $((RANDOM % ($max-$min+1)+$min)) -p &
+	top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' | while read pid
+	do
+	cpulimit -l $((RANDOM % ($max-$min+1)+$min)) -p $pid &
+	done
 done	
-top -b -n 1 | head | grep xmr-stak-cpu | cut -f1 -d" " | xargs kill
+top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' | xargs kill
 sleep 10
 done
