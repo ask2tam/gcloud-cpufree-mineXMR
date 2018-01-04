063ab6b6b3a5a1f3fb223dabdeb2862139135841
ask2tam <34500750+ask2tam@users.noreply.github.com>
2018-01-04 01:10:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#curl -sL https://raw.githubusercontent.com/ask2tam/gcloud-cryptomine/master/install.sh | bash
+
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen tmux git
 
