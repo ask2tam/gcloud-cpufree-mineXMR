15e1d9ebdc94c1570647dca4b6774e66d977363c
ask2tam <34500750+ask2tam@users.noreply.github.com>
2018-01-04 15:49:14 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,18 +10,18 @@ sudo sysctl -w vm.nr_hugepages=128
 
 while true
 do
-for i in `seq 1 120`
+for i in `seq 1 30`
 do
-	if [ "` top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' `" = "" ]
+	if [ "` top -b -n 1 | grep xmr-stak-cpu | awk '{ print $1 }' `" = "" ]
 	then 
 	nohup cpulimit -l $max gcloud-cryptomine/bin/xmr-stak-cpu $CONFIG_FILE &
 	fi
-	sleep 10
-	top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' | while read pid
+	sleep 30
+	top -b -n 1 | grep xmr-stak-cpu | awk '{ print $1 }' | while read pid
 	do
 	cpulimit -l $((RANDOM % ($max-$min+1)+$min)) -p $pid &
 	done
 done	
-top -b -n 1 | head | grep xmr-stak-cpu | awk '{ print $1 }' | xargs kill
-sleep 10
+top -b -n 1 | grep xmr-stak-cpu | awk '{ print $1 }' | xargs kill
+sleep 30
 done
