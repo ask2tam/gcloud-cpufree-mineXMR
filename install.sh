89acb96968c03b9e66d7eebce4da2c845b331303
ask2tam <34500750+ask2tam@users.noreply.github.com>
2018-01-04 01:06:22 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen tmux git
+
+git clone https://github.com/ask2tam/cpulimit
+cd cpulimit
+make
+sudo cp src/cpulimit /usr/bin
+cd ..
+git clone https://github.com/ask2tam/gcloud-cryptomine
+cd gcloud-cryptomine
+cmake .
+make install
+cd ..
+
+sudo sysctl -w vm.nr_hugepages=128
+ulimit -l
+sudo sh -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
